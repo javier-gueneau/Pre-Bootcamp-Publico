30f48acfa03f464e3ff8616884f681d5b449d37e
Javier Gueneau <javiergueneau@gmail.com>
2022-06-25 12:31:56 -0400

    Add files via upload

---
+++ w3-3-script.js
@@ -0,0 +1,7 @@
+var prueba=document.querySelector(".prueba")
+var num=3;
+
+function increaseLike() {
+    num+=1;
+    prueba.innerHTML=num + " Likes";
+    }
---
+++ w3-3b-network.js
@@ -0,0 +1,7 @@
+var like1=document.querySelector("#like1")
+var num=3;
+
+function increaseLike() {
+    num+=1;
+    like1.innerHTML=num + " Likes";
+    }
